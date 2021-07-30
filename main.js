d790df4853d39ec2b460f070e39a207349c76e19
quocthinh <thinhvqps11484@fpt.edu.vn>
2021-07-30 22:02:21 +0700

    das

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const jsonServer = require("json-server");
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
 
 // Add custom routes before JSON Server router
-server.get("/echo", (req, res) => {
+server.get('/echo', (req, res) => {
     res.jsonp(req.query);
 });
 
@@ -15,7 +15,7 @@ server.get("/echo", (req, res) => {
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
-    if (req.method === "POST") {
+    if (req.method === 'POST') {
         req.body.createdAt = Date.now();
     }
     // Continue to JSON Server router
@@ -23,7 +23,8 @@ server.use((req, res, next) => {
 });
 
 // Use default router
+const PORT = process.env.POST || 3000;
 server.use(router);
-server.listen(3000, () => {
-    console.log("JSON Server is running");
+server.listen(PORT, () => {
+    console.log('JSON Server is running');
 });
