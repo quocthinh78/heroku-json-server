010a67fda58dcc3780513548a34c5602a8636cef
quocthinh <thinhvqps11484@fpt.edu.vn>
2021-07-30 22:08:31 +0700

    das

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ server.use((req, res, next) => {
 });
 
 // Use default router
-const PORT = process.env.POST || 3000;
+const PORT = process.env.PORT || 3000;
 server.use(router);
 server.listen(PORT, () => {
     console.log('JSON Server is running');
